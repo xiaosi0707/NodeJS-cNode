122f05bb567d8b92f166bd45b06a2518cc148c31
wyunfei <583514368@qq.com>
2019-02-11 19:47:10 +0800

    表单数据同时要存在

--- routes/router.js
+++ routes/router.js
@@ -62,7 +62,8 @@ router.post('/register', (req, res) => {
 
             // 这种处理方式不代表不好，服务端处理起来更加安全一些，但是会增大服务器的压力，同时为了交互的一致性，因为有些浏览器禁用了ajax或不支持ajax，GitHub目前就是这么做的，从服务端返回提示消息
             return res.render('register.html', {
-                err_message: '邮箱或昵称已存在'
+                err_message: '邮箱或昵称已存在',
+                formData: body
             })
         }
 
